25bf5b67056be8d52727fe6d672f3ab53c34322c
sandeepmernstack <sandeep9682980@gmail.com>
2024-12-16 17:49:06 +0530

    update butto text

--- 01_react/src/Bgchanger.jsx
+++ 01_react/src/Bgchanger.jsx
@@ -6,12 +6,12 @@ const Bgchanger = () => {
     <div className='main-container' style={{backgroundColor:color}}>
        <div className="buttons-container">
           <button onClick={()=>setColor("red")} style={{backgroundColor:"red"}}>Red</button>
-          <button onClick={()=>setColor("green")} style={{backgroundColor:"green"}}>Red</button>
-          <button onClick={()=>setColor("blue")} style={{backgroundColor:"blue"}}>Red</button>
-          <button onClick={()=>setColor("pink")} style={{backgroundColor:"pink"}}>Red</button>
-          <button onClick={()=>setColor("yellow")} style={{backgroundColor:"yellow"}}>Red</button>
-          <button onClick={()=>setColor("cyan")} style={{backgroundColor:"cyan"}}>Red</button>
-          <button onClick={()=>setColor("orange")} style={{backgroundColor:"orange"}}>Red</button>
+          <button onClick={()=>setColor("green")} style={{backgroundColor:"green"}}>Green</button>
+          <button onClick={()=>setColor("blue")} style={{backgroundColor:"blue"}}>Blue</button>
+          <button onClick={()=>setColor("pink")} style={{backgroundColor:"pink"}}>Pink</button>
+          <button onClick={()=>setColor("yellow")} style={{backgroundColor:"yellow"}}>Yellow</button>
+          <button onClick={()=>setColor("cyan")} style={{backgroundColor:"cyan"}}>Cyan</button>
+          <button onClick={()=>setColor("orange")} style={{backgroundColor:"orange"}}>Orange</button>
        </div>
     </div>
   )
